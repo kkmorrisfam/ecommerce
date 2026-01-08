a71ecb72b104fc75fc24c49b07cd2a1c5a1dfa7b
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-08 07:41:53 -0800

    add tracking page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 import './App.css'
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>      
     </>
   )
