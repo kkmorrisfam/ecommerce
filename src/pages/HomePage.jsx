1437d14d1bf3961c64de248cb5abc7574f8075dc
Kerri Morris <kkmorrisfam@hotmail.com>
2026-01-08 07:19:46 -0800

    add checkout page code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import './HomePage.css';
 export function HomePage() {
     return (
       <>        
+        <title>My Store</title>
         <div className="header">
         <div className="left-section">
             <a href="index.html" className="header-link">
